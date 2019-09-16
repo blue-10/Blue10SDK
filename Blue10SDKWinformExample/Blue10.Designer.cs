bb1f62054816272944adf7277069d1afe3d301ab
John Zuijderwijk <john@zind.nl>
2019-09-16 20:05:11 +0200

    Blue10SDK Winform

---
+++ Blue10SDKWinformExample/Blue10.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Blue10SDKWinformExample
+{
+    partial class Blue10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.programTabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.programTabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // programTabs
+            // 
+            this.programTabs.Controls.Add(this.tabPage1);
+            this.programTabs.Controls.Add(this.tabPage2);
+            this.programTabs.Location = new System.Drawing.Point(12, 12);
+            this.programTabs.Name = "programTabs";
+            this.programTabs.SelectedIndex = 0;
+            this.programTabs.Size = new System.Drawing.Size(1073, 667);
+            this.programTabs.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "companyTab";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1065, 641);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "loging/company";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1065, 641);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Blue10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 691);
+            this.Controls.Add(this.programTabs);
+            this.Name = "Blue10";
+            this.Text = "Blue10 Api";
+            this.programTabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl programTabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
--- Blue10SDKWinformExample/Blue10.cs
+++ Blue10SDKWinformExample/Blue10.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Blue10SDKWinformExample
 {
-    public partial class Form1 : Form
+    public partial class Blue10 : Form
     {
-        public Form1()
+        public Blue10()
         {
             InitializeComponent();
         }
