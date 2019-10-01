1c414b213139bdd55a31821188c67cb7b094a6b0
John Zuijderwijk <jzuijderwijk@blue10.nl>
2019-10-01 12:10:15 +0200

    VendorModel extended

--- Blue10SDK/Model/Vendor.cs
+++ Blue10SDK/Model/Vendor.cs
@@ -10,6 +10,10 @@ namespace Blue10SDK
         public List<string> iban { get; set; }
         public string currency_code { get; set; } = "EUR";
         public string vendor_customer_code { get; set; }
+        public string default_ledger_code { get; set; } = string.Empty;
+        public string default_vat_code { get; set; } = string.Empty;
+        public string default_vat_scenario_code { get; set; } = string.Empty;
+        public string default_payment_term_code { get; set; } = string.Empty;
         public bool blocked { get; set; }
     }
 }
