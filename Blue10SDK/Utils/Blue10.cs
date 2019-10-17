d3953a922be3ac1373cb09e07bd45668cb155b56
Yasen Dinkov <ydinkov@blue10.nl>
2019-10-17 11:36:13 +0200

    Added service extensions

---
+++ Blue10SDK/Utils/Blue10.cs
@@ -0,0 +1,37 @@
+using Blue10SDK;
+using Microsoft.Extensions.DependencyInjection;
+    
+namespace Blue10SDK
+{
+    public static class Blue10
+    {
+        public static IBlue10Client CreateClient(string blue10ApiKey) =>
+            new ServiceCollection()                    
+                .AddHttpClient<B10WebApiAdapter>(client =>
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"access_token {blue10ApiKey}");
+                        
+                }).Services
+                .AddSingleton<IWebApiAdapter, B10WebApiAdapter>()
+                .AddSingleton<IBlue10Client, Blue10Desk>()
+                .BuildServiceProvider()
+                .GetService<IBlue10Client>();
+    
+
+
+    public static void AddBlue10(this IServiceCollection services, string blue10ApiKey)
+    {
+       services
+           .AddHttpClient<B10WebApiAdapter>(client =>
+           {
+               client.DefaultRequestHeaders.Add("Authorization", $"access_token {blue10ApiKey}");
+           }).Services
+           .AddSingleton<IWebApiAdapter, B10WebApiAdapter>()
+           .AddSingleton<IBlue10Client, Blue10Desk>()
+           .BuildServiceProvider()
+           .GetService<IBlue10Client>();
+    }
+    
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Blue10SDK
-A .Net Standard Library used to communicate with the Blue10 Web API
+![alt text](https://login.blue10.com/Content/images/Blue10-Logo-RGB-156.png "Logo Title Text 1")
+# Blue10 SDK
+
+This is the Blue 10 .net core SDK.
+
+
+## Request an environment at Blue10
+
+1. To start useing Blue10 you first need to create and environment.
+[Contact Blue 10](https://www.blue10.com/contact/).
+2. After an environment has been created Request an API Key.
+During the API key creation process our staff will assess which features your application requires and will attatch that feature set to your personal environment's API Key.
+3. Once you have a personal API Key, you can get started with the SDK.
+
+
+## Getting started
+
+For this example we will create a simple _console_ adapter that synchronises vendor information.
+
+1. ### Import master data into Blue10
+
+Create a new  visual studio project and reference the blue10 project sdk.
+//Later invullen
+
+in the main Program.cs you will see this:
+ ```cs
+using System;
+
+namespace GettingStartedWithBlue10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+ ```
+
+
+
+
+ 
