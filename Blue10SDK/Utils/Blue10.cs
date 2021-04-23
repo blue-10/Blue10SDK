0cc6c2f8d9b016ce66258ae002c90363a8d2403a
Marijn <mdenhaan@blue10.nl>
2021-04-23 08:59:33 +0200

    Add option to add B10sdk service with HttpClientFactory

--- Blue10SDK/Utils/Blue10.cs
+++ Blue10SDK/Utils/Blue10.cs
@@ -1,29 +1,44 @@
-using System;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Blue10SDK.Utils
 {
     public static class Blue10
     {
-        const string BLUE10_API_URL = "https://api.blue10.com/v2/";
+        internal const string BLUE10_API_URL = "https://api.blue10.com/v2/";
 
         public static IBlue10Client CreateClient(string blue10ApiKey, string url = BLUE10_API_URL) =>
-             new Blue10Desk(CreateAsyncClient(blue10ApiKey,url));
-            
-        
+             new Blue10Desk(CreateAsyncClient(blue10ApiKey, url));
+
+
         public static IBlue10AsyncClient CreateAsyncClient(string blue10ApiKey, string url = BLUE10_API_URL) =>
-            new ServiceCollection().AddBlue10(blue10ApiKey,url).BuildServiceProvider().GetService<IBlue10AsyncClient>();
+            new ServiceCollection().AddBlue10(blue10ApiKey, url).BuildServiceProvider().GetService<IBlue10AsyncClient>();
 
         public static IServiceCollection AddBlue10(this IServiceCollection services, string blue10ApiKey, string url = BLUE10_API_URL)
         {
+
+            return AddBlue10(services, (provider, configuration) =>
+            {
+                configuration.ApiKey = blue10ApiKey;
+                configuration.Url = url;
+            });
+        }
+
+        public static IServiceCollection AddBlue10(this IServiceCollection services, Action<IServiceProvider, Blue10SDKConfiguration> configureClient)
+        {
             services
-                .AddHttpClient<B10WebApiAdapter>(client =>
-                {
-                    client.BaseAddress = new Uri(uriString: url);
-                    client.DefaultRequestHeaders.Add("Authorization", $"access_token {blue10ApiKey}");
-                }).Services
                 .AddSingleton<IWebApiAdapter, B10WebApiAdapter>()
                 .AddSingleton<IBlue10AsyncClient, Blue10AsyncDesk>();
+
+            services.AddHttpClient<B10WebApiAdapter>((provider, client) =>
+            {
+                var blue10SDKConfiguration = new Blue10SDKConfiguration();
+                configureClient(provider, blue10SDKConfiguration);
+
+                client.BaseAddress = new Uri(blue10SDKConfiguration.Url);
+                client.DefaultRequestHeaders.Add("Authorization", $"access_token {blue10SDKConfiguration.ApiKey}");
+            });
+
             return services;
         }
     }
---
+++ Blue10SDK/Utils/Blue10SDKConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Blue10SDK.Utils
+{
+    public class Blue10SDKConfiguration
+    {
+        public string ApiKey { get; set; }
+        public string Url { get; set; } = Blue10.BLUE10_API_URL;
+
+    }
+}
