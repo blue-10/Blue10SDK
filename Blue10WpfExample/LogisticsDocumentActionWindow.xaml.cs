b650cd81f54155bd26e3014d2dd816027abdfcb0
John Zuijderwijk <jzuijderwijk@blue10.nl>
2021-07-08 16:49:22 +0200

    Stkuje verder

--- Blue10WpfExample/LogisticsDocumentActionWindow.xaml.cs
+++ Blue10WpfExample/LogisticsDocumentActionWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using Blue10SDK;
 using Blue10SDK.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Blue10SdkWpfExample
 {
@@ -29,9 +19,6 @@ namespace Blue10SdkWpfExample
             ExportInvoiceToLogisticsAdapterTab.Visibility = Visibility.Hidden;
             GetMatchInvoiceTab.Visibility = Visibility.Hidden;
 
-            //,
-            //,
-            //
             switch (DocAction.Action)
             {
                 case EDocumentAction.create_logistics_purchase_invoice:
@@ -49,22 +36,18 @@ namespace Blue10SdkWpfExample
         private B10DeskHelper B10DH { get; set; }
         private LogisticsDocumentAction DocAction { get; set; }
 
-
         private void FillCreatePurchaseInvoiceTab()
         {
             CreateInvoiceInLogisticsAdapterTab.Visibility = Visibility.Visible;
             CreateInvoiceInLogisticsAdapterTab.IsSelected = true;
             var fInvoice = DocAction.PurchaseInvoice;
+            DocAction.Message = "gezet";
             CreatePurchaseInvoiceText.Text = $"Invoice: {fInvoice.Blue10Code}, Company: {fInvoice.IdCompany}, Vendor: {fInvoice.VendorCode}, net: {fInvoice.NetAmount.ToString()}, gross: {fInvoice.GrossAmount}, vat: {(fInvoice.GrossAmount - fInvoice.NetAmount)}";
         }
        
         private async void FinishCreatePurchaseInvoice(object sender, RoutedEventArgs e)
         {
-            //DocAction.PurchaseInvoice.AdministrationCode = CreatePurchaseInvoiceAdminitrationCode.Text;
-            //if (CreatePurchaseInvoiceDueDate.SelectedDate != null)
-            //{
-            //    DocAction.PurchaseInvoice.PaymentDueDate = (DateTime)CreatePurchaseInvoiceDueDate.SelectedDate;
-            //}
+            DocAction.Message = CreatePurchaseInvoiceMessage.Text;
             DocAction.Status = "done";
             DocAction.Result = "success";
             await B10DH.SaveLogisticsDocumentAction(DocAction);
@@ -78,7 +61,6 @@ namespace Blue10SdkWpfExample
             var fInvoice = DocAction.PurchaseInvoice;
             MatchPurchaseInvoiceText.Text = $"Invoice: {fInvoice.AdministrationCode} / {fInvoice.Blue10Code}, Company: {fInvoice.IdCompany}, Vendor: {fInvoice.VendorCode}, net: {fInvoice.NetAmount.ToString()}, gross: {fInvoice.GrossAmount}, vat: {(fInvoice.GrossAmount - fInvoice.NetAmount)}";
             DocAction.PurchaseInvoice.InvoiceLines = new List<InvoiceLine>() { };
-            //InvoiceLineGrid.ItemsSource = DocAction.PurchaseInvoice.InvoiceLines;
             InvoiceLineGrid.ItemsSource = new List<InvoiceLine>() { new InvoiceLine() { Description = "doei" } };
         }
 
